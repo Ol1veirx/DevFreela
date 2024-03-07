e12716e23368bdf2fcf0db78994cc6d71c2c4065
Oliveira <liedsonwork@hotmail.com>
2024-03-07 10:48:44 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    internal class User : BaseEntity
+    {
+        public User(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+
+            CreatedAt = DateTime.Now;
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; private set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProjects {  get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+    }
+}
